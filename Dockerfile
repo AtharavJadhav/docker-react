57d0917f6e716cd6e08f6b40922bc29740f24868
Atharav <bt21cse017@students.vnit.ac.in>
2023-05-22 11:13:28 +0530

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM node:alpine as builder
+WORKDIR '/app'
+# Copy package.json first to avoid unnecessary npm installs
+COPY package.json .
+RUN npm install
+COPY . .
+RUN npm run build
+
+# Path: Dockerfile
+# Compare this snippet from Dockerfile.dev:
+FROM nginx
+COPY --from=0 /app/build /usr/share/nginx/html
--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi There! I am <code>src/App.js</code> and I am a React App.
         </p>
         <a
           className="App-link"
